7d14db07a1063b6232e17cc300d9d111f1d3d9e4
fernando-adrian <advancexz@gmail.com>
2021-07-12 14:40:06 -0700

    routing created for /api/productos

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const app = express();
 //conectamos a la BD
 conectarDB();
 
+app.use('/api/productos', require('./routes/producto'));
 //definimos la ruta principal
-app.get('/', (req, res)=> {
-    res.send('Hola mundo');
-});
+// app.get('/', (req, res)=> {
+//     res.send('Hola mundo');
+// });
 
 app.listen (4000, ()=> {
     console.log("el servidor esta corriendo perfectamente");
---
+++ routes/producto.js
@@ -0,0 +1,10 @@
+//rutas para producto
+const express = require('express');
+const router = express.Router();
+
+// api/productos
+router.post('/', () => {
+    console.log('creando producto...');
+});
+
+module.exports = router;
