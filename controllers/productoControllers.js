6d42a5016ab48aeffe5031d7fa747e57f3448b6f
fernando-adrian <advancexz@gmail.com>
2021-07-12 16:56:07 -0700

    crearProducto waits for theobject to be send, to save it on the mongoDB collection

--- controllers/productoControllers.js
+++ controllers/productoControllers.js
@@ -1,5 +1,17 @@
+const Producto = require("../models/Producto");
 
+exports.crearProducto = async (req, res)=> {
+    
+    try{
+        let producto;
 
-exports.crearProducto = (req, res)=> {
-    console.log(req.body);
+        producto = new Producto(req.body);
+
+        await producto.save();
+        res.send(producto);
+
+    } catch(error){
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
 }
