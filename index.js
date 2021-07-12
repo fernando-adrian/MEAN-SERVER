a019b714ab7a817457e2c335f6ff36b28e96ed71
fernando-adrian <advancexz@gmail.com>
2021-07-12 12:15:04 -0700

    server express created, 'hola mundo' is returned on localhost:4000

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("desde index.js")
+const express = require('express');
+
+//creamos el servidor
+const app = express();
+
+//definimis la ruta principal
+app.get('/', (req, res)=> {
+    res.send('Hola mundo');
+});
+
+app.listen (4000, ()=> {
+    console.log("el servidor esta corriendo perfectamente");
+});
